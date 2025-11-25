9c98656f1dab7ebad88889e50666823406995793
User <User@LAPTOP-7039273T>
2025-11-25 17:05:15 +0530

    second commit

---
+++ src/test/java/Practice/GitPractice2.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class GitPractice2 {
+
+	@Test
+	public void m1() {
+	System.out.println("hello");
+	System.out.println("hi"); 
+	}
+}
